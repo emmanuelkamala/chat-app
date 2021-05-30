e4ef3dcb115cfd9d63d239250ae94999a725f4d4
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-05-30 23:05:19 +0300

    Set up google and facebook auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+import 'firebase/app';
+
+import { auth } from '../firebase';
+import firebase from 'firebase/app';
 
 export default function Login() {
     return (
         <div id='login-page'>
             <div id='login-card'>
                 <h2>Welcome to Chat App</h2>
-                <div className='login-button google'>
+                <div className='login-button google'
+                    onClick = { () => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                >
                     <GoogleOutlined /> Sign In With Google
                 </div> 
                 <br /><br />
-                <div className='login-button facebook'>
+                <div className='login-button facebook'
+                    onClick = { () => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
+                >
                     <FacebookOutlined /> Sign In With Facebook
                 </div>
             </div>
