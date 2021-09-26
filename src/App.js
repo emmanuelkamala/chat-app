c56837a681dd81ea905f4feb35b1cd306fdf691c
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-27 02:36:10 +0300

    Edit app file

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Chat application
-    </div>
+    <ChatEngine 
+      height="100vh"
+      projectID="9d45dd29-33f2-4919-8340-19bcaf6a2e77"
+      userName="ejoka"
+      userSecret="tanzania"
+    />
   );
 }
 
