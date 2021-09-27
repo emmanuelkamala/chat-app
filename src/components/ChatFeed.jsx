a754e4ddc13ff77814c6469617fd4959637054c0
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-27 09:02:04 +0300

    Add logic to chat feed component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,9 +1,45 @@
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-  console.log(props);
+  const { chats, activeChat, userName, messages } = props;
+
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+    
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = username === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            {
+              isMyMessage ? <MyMessage /> : <TheirMessage />
+            }
+          </div>
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : 0, marginLeft: isMyMessage ? 0 : '68px' }}>
+            read receipts
+          </div>
+        </div>
+      )
+    })
+  }
+
+  renderMessages();
+  
+  if (!chat) return 'Loading...'
+  
   return (
-    <div>
-      Chat Feed
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">
+          {chat.title}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,10 @@
+
+const MyMessage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyMessage;
